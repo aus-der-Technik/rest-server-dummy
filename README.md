fd012173b9dc050e94a9141ca29911165b51b018
Kris <kris@opentabs.de>
2015-02-27 18:13:03 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,161 @@
+# The ultimate restify dummy server #
+
+**If you are developing a REST-Client than you definitely  need this mock server**
+
+## Installation ##
+
+```bash
+npm install rest-server-dummy -g
+```
+
+## What It Does for You ##
+
+When you are dealing with remote REST API's as a developer of a client Application, than you should have *something* on your local machine that mocks the API. 
+
+**rest-server-dummy** is the tool you want to debug GET, POST  PUT and DELETE calls in the easiest way. Here is the  simple elegance of stupidity that just works. It will save you from a lot of trouble while you developing on your software.
+
+
+## How To Use It ##
+
+After you have installed the Software, you should start the server:
+
+```bash
+node rest-server-dummy
+```
+
+The default server settings are:
+
+|:----------|----------:|  
+|PORT | 8080 |  
+|DIRECTORY | ./ |  
+
+Now you can work with the server in your application with the url 
+
+```
+http://localhost:8080/some/great/file.json
+```
+
+### POST ###
+
+Everytime you post a document to the server it will create a file in the DIRECTORY. If you are working with the default setting, and post /some/great/file.json, than a directory called "some" will be created in your current work directory. Inside that directory an other one named "great" is created and inside this a file named "file.json" stores the content from your POST Request. 
+
+To explain further examples, we asume at this point that you post a document that looks like this: 
+
+```json
+{
+   'name': 'james'
+}
+```
+
+It is not very complex but you will know better what fits your needs and can build your own document. 
+
+### PUT ###
+
+A PUT will alter a document. For example we a document to the same address: some/great/file.json
+
+```json
+{
+   'gender': 'male'
+}
+```
+
+Than the result in the file will be merged to:
+
+```json
+{
+    'name': 'james',
+    'gender': 'male'
+}
+```
+
+### GET ###
+
+You can fetch posted documents, or you can copy mock .json files to the directory structure and fetch them with a GET Request.
+
+A GET on some/great/file.json will return the same document: 
+
+```json
+{
+    'name': 'james',
+    'gender': 'male'
+}
+```
+
+
+### DELETE ###
+
+If you request a DELETE method, the file, and the directories will be removed. A GET after a DELETE should produce a 404.
+
+The directories will be removed when there is no other file only. Even a .response file will halt the removing process!
+
+While deleting a resource no request file is produced. 
+
+## The .request File ##
+
+Every call will produce a extra file with the suffix .request If you are GET/POST/PUT a resourcece (for example:  some/great/file.json), a file named  some/great/file.request will be written. In this file your request headers are stored. 
+
+```json
+{
+  "host": "localhost:8080",
+  "accept": "*/*",
+  "content-type": "application/x-www-form-urlencoded",
+  "connection": "keep-alive",
+  "user-agent": "YourApp/0.1",
+  "accept-language": "en-us",
+  "accept-encoding": "gzip, deflate",
+  "content-length": "7"
+}
+```
+
+This is very handy when you debugging your client. All headers are stored right beside the content.
+
+## Extra Options ##
+
+Maybe you do not want the results in the working directory? No Problem, just start up the server with a DIRECTORY option and all files are stored there:
+
+```bash
+DIRECTORY=/tmp node rest-server-dummy
+```
+
+## Mastering Your Calls ##
+
+Debugging clients is hard. But not as hard as [Haddekuche](http://en.wikipedia.org/wiki/Haddekuche)! 
+
+To fetch the document with additional headers, you can write a .response file with the same baseman as your resource and store some headers there. 
+In our example a file named:  some/great/file.response will be merged to the headers that are returned to your client. 
+
+```json
+{
+  "foo": "bar"
+}
+```
+
+That response file will produce a header like: 
+
+```
+Content-Type: application/json
+Access-Control-Allow-Origin: *
+Access-Control-Allow-Methods: GET, POST, PUT, DELETE
+Server: rest-server-dummy
+Access-Control-Expose-Headers: Api-Version, Request-Id, Response-Time
+Api-Version: 1.0.0
+Content-MD5: TGOyctqnSLSYozRZIPJyyQ==
+Access-Control-Allow-Headers: Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, Api-Version, Response-Time
+Response-Time: 15
+Date: Fri, 27 Feb 2015 16:31:56 GMT
+Content-Length: 126
+Connection: Keep-Alive
+Request-Id: 25570e50-be9e-11e4-b5c2-235a2c2e6d6c
+foo: bar
+```
+
+## Spend a Sartoschi ##
+if you like this tool, spend a Sartoschi to this address:
+
+```
+1DtvkCh28zqarTEUHtxs7gWtutsv2Cnf9d
+```
+
+
+
+
---
+++ rest-server-dummy.js
@@ -0,0 +1,219 @@
+#!/usr/bin/env node 
+
+/**
+# ----------------------------------------
+# rest-server-dummy
+# ----------------------------------------
+# Dummy restserver to test GET, POST and 
+# PUT in your client.
+# 
+# ausdertechnik.de 2015 
+# ----------------------------------------
+*/
+
+// require the project modules 
+var   path 					= require('path')
+	, fs 					= require('fs')
+	, events 				= require('events')
+	, async					= require('async')
+	, _						= require('underscore')
+	, S						= require('string')
+	, restify 				= require('restify')
+	, mkdirp 				= require('mkdirp')
+	;
+	
+/**
+ * Global eventEmitter
+ * @var object eventEmitter
+ */
+var eventEmitter = 		new events.EventEmitter();
+
+/** 
+ * The REST-Server
+ * @var object server 
+ */
+var server = restify.createServer(_.extend({
+	'acceptable': 'application/json'
+},{
+    "name": "restsserver",
+    "version": ["1.0.0"],
+    "throttle": {
+        "rate": 10,
+        "burst": 10,
+        "ip": false,
+        "xff": true
+    }
+}));
+
+// pass X-Domain check 
+server.use(
+  function crossOrigin(req,res,next){
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    return next();
+  }
+);
+
+// Configure the server
+server.use(restify.authorizationParser());
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+server.use(restify.CORS({origins: ['*']}));
+server.use(restify.fullResponse());
+
+server.pre(restify.pre.userAgentConnection());
+
+
+// set response header
+server.use(function(req, res, next){
+	var directory = (documentDirectory + req.url).split(path.sep);
+	directory.pop();
+	var filename = directory.join(path.sep) + '/' + path.basename(req.url, path.extname(req.url)) + '.response';
+	fs.readFile( filename, {encoding: "utf-8"}, function (err, data) {
+		if (err) return next();
+		var headers = JSON.parse(data);			
+		_.each(_.keys(headers), function(key){
+			res.header(key, headers[key]);		
+		});
+		next();
+	});
+});
+
+// Error handling
+server.on('uncaughtException', function error(req, res, route, err){
+	console.log("ROUTE", route);
+	console.log("ERROR", err);
+	var ERR = require("async-stacktrace");
+	ERR(err, function callback(stack){
+		// log to "told log recorder"
+	 	console.error(stack.toString(), stack.stack);
+	});
+});
+
+// ROUTES
+var documentDirectory = process.env.DIRECTORY || process.cwd();
+
+server.get('(.*)\.json'
+	, function(req, res, next){
+		eventEmitter.emit('write::headers', req.url, _.clone(req));
+		fs.exists( documentDirectory + req.url, function documentIsAvailabel(exists){
+			if(exists){ 
+				fs.readFile( documentDirectory + req.url, {encoding: "utf-8"}, function (err, data) {
+					if (err) return res.send(500, err);
+					res.send(200, JSON.parse(data));
+					return next();					
+				});
+			} else {
+				return res.send(404);
+			}
+		});
+	}	
+);
+
+server.post('(.*)\.json'
+	, function(req, res, next){
+		eventEmitter.emit('write::headers', req.url, _.clone(req));
+		if( typeof req.body === 'string'){
+			req.body = JSON.parse(req.body);
+		}
+		fs.writeFile( documentDirectory + req.url, JSON.stringify(req.body, null, 2), {encoding: "utf-8"}, function (err, data) {
+			if (err) return res.send(500, err);
+			res.send(201, req.body);
+			return next();					
+		});
+	}	
+);
+
+server.put('(.*)\.json'
+	, function(req, res, next){ 
+		eventEmitter.emit('write::headers', req.url, _.clone(req));
+		var document = {};
+		fs.exists( documentDirectory + req.url, function documentIsAvailabel(exists){
+			async.waterfall([
+				function(callback){
+					if(exists){ 
+						fs.readFile( documentDirectory + req.url, {encoding: "utf-8"}, function (err, data) {
+							if (err) return callback(err, null);	
+							return callback(null, JSON.parse(data));	
+						});
+					} else {
+						return callback(null, {});
+					}
+				}
+				, function(results, callback){
+					if( typeof req.body === 'string'){
+						req.body = JSON.parse(req.body);
+					}
+					var data = _.extend(req.body, results || {} );
+					
+					fs.writeFile( documentDirectory + req.url, JSON.stringify(data, null, 2), {encoding: "utf-8"}, function (err) {
+						if (err) return callback(err);
+						return callback(null, data);		
+					});
+				}
+			], function(err, result){
+				if(err) return res.send(500, err);
+				res.send(200, result);
+			});		
+		});
+	}	
+);
+
+server.del('(.*)\.json'
+	, function(req, res, next){ 
+		eventEmitter.emit('write::headers', req.url, _.clone(req));
+		var document = {};
+		fs.exists( documentDirectory + req.url, function documentIsAvailabel(exists){
+			if(exists == false) return res.send(404);
+			fs.unlink(documentDirectory + req.url, function(err){
+				if(err) return res.send(500, err);
+				var directory = (documentDirectory + req.url).split(path.sep);
+				directory.pop();
+				var filename = directory.join(path.sep) + '/' + path.basename(req.url, path.extname(req.url)) + '.request';				
+				fs.unlink(filename, function(err){
+					eventEmitter.emit('remove::directorytree', directory.join(path.sep));
+				});
+				res.send(200);
+			});
+		})
+	}
+);
+	
+
+/** 
+ * EVENTS
+ */
+// write header
+eventEmitter.on('write::headers', function(url, data){
+	var directory = (documentDirectory + url).split(path.sep);
+	directory.pop();
+	var filename = directory.join(path.sep) + '/' + path.basename(url, path.extname(url)) + '.request';
+	mkdirp(directory.join(path.sep), function (err) {
+		if(err) return;
+		fs.writeFile(filename, JSON.stringify(data.headers, null, 2), null);
+	});
+});
+
+eventEmitter.on('remove::directorytree', function(directory){
+	var rmdir = function rmdir(directory){
+		var directories = directory.split(path.sep);
+		fs.readdir(directories.join(path.sep), function ls(err, files){
+			files = _.filter(files, function(file){
+				if(file != '.' && file != '..') return true; 
+				return false;
+			});
+			if(files.length == 0) {
+				fs.rmdir(directory, function(err){
+					directories.pop();
+					rmdir(directories.join(path.sep));
+				});			
+			}
+		});
+	};
+	rmdir(directory);
+});
+
+// START
+server.listen(process.env.PORT || 8080, function() {
+	console.log('%s listening at %s', server.name, server.url);
+});
+
